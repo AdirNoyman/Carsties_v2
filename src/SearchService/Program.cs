a3601e55a12154792cedd8e113507e6c0013373b
Adir Noyman <ofahol2007@hotmail.com>
2023-12-30 13:08:15 +0200

    Added searching and filtering functionality

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,5 +1,6 @@
 using MongoDB.Driver;
 using MongoDB.Entities;
+using SearchService.Data;
 using SearchService.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,16 +9,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
 
-// Set Index for search functionality
-await DB.Index<Item>()
-.Key(x => x.Make, KeyType.Text)
-.Key(x => x.Model, KeyType.Text)
-.Key(x => x.Color, KeyType.Text)
-.CreateAsync();
 
 var app = builder.Build();
+
+// Initialize the database
+try
+{
+    DbInitializer.InitDb(app).Wait();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
 
 
 // Configure the HTTP request pipeline. Here we add the middleware to handle the authentication.
